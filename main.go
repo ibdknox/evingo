d64281c7b02ece84054f3eee459ebaf93a090659
Joshua Cole <joshuafcole@gmail.com>
2016-05-11 20:33:56 -0700

    Test shim for queryGraph in main

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
-	"bufio"
+	//"bufio"
 	"fmt"
 	"github.com/witheve/evingo/value"
-	"os"
-	"strings"
+	//"os"
+	//"strings"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		line := scanner.Text()
-		_ = strings.Split(line, " ")
-	}
+	var s = "[[\"a\", \"attr1\", 7], [\"b\", \"attr1\", \"dog\"], [\"b\", \"attr2\", true]]"
+	var b = []byte(s)
+	fmt.Println(ReadFactsFromJson(b))
+
+	//scanner := bufio.NewScanner(os.Stdin)
+	//for scanner.Scan() {
+	//	line := scanner.Text()
+	//	_ = strings.Split(line, " ")
+	//}
 
 	// genericize
 	tree := value.NewMapNode()
