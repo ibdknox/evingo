1a083fa09eb3a368f7a95c3e5587c790cc4e0747
Joshua Cole <joshuafcole@gmail.com>
2016-05-11 20:33:27 -0700

    Fix broken String implementations and add NewNumberFrom* and NewBoolean

--- value/value.go
+++ value/value.go
@@ -46,55 +46,72 @@ func (t Text) Hash() uint32 {
 }
 
 func (t Text) String() string {
-	return ""
+	return t.s
 }
 
 func NewText(s string) Value {
 	return &Text{s}
 }
 
-type number struct {
+type Number struct {
 	d decimal.Decimal
 }
 
-func (n number) Equal(v Value) bool {
-	if t2, ok := v.(number); ok {
+func (n Number) Equal(v Value) bool {
+	if t2, ok := v.(Number); ok {
 		return n.d == t2.d
 	}
 	return false
 }
 
-func (n number) Hash() uint32 {
+func (n Number) Hash() uint32 {
 	return 0
 }
 
-func (n number) String() string {
+func (n Number) String() string {
 	return n.d.String()
 }
 
-type boolean struct {
+func NewNumberFromFloat(n float64) Value {
+	return &Number{decimal.NewFromFloat(n)}
+}
+func NewNumberFromInt(n int64) Value {
+	return &Number{decimal.New(n, 1)}
+}
+func NewNumberFromString(n string) Value {
+	var d, err = decimal.NewFromString(n)
+	if err != nil {
+		panic("Invalid decimal string: " + n)
+	}
+	return &Number{d}
+}
+
+type Boolean struct {
 	b bool
 }
 
-func (b boolean) Equal(v Value) bool {
-	if b2, ok := v.(boolean); ok {
+func (b Boolean) Equal(v Value) bool {
+	if b2, ok := v.(Boolean); ok {
 		return b.b == b2.b
 	}
 	return false
 
 }
 
-func (n boolean) Hash() uint32 {
-	if n.b {
+func (b Boolean) Hash() uint32 {
+	if b.b {
 		return 1
 	}
 	return 0
 }
 
-func (n boolean) String() string {
-	if n.b {
+func (b Boolean) String() string {
+	if b.b {
 		return "true"
 	}
 	return "false"
+}
 
+func NewBoolean(b bool) Value {
+	return &Boolean{b}
 }
